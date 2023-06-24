f4c7d08b00c35af98e7fbfe6ca40c00ba0792c9b
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-24 14:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var student = {
+  name : "Rahul",
+  age : 22
+}
+Object.prototype.getKeys = function () {
+  var a = [];
+  for(var i in this) {
+    a.push(i);
+  }
+  return a;
+}
